1ea121f2fd3d839bb8ecdd7b7493ebd661703408
Dylan <dburston0@gmail.com>
2022-02-13 19:35:48 -0500

    add post comment models and FULL CRUD

---
+++ main_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('posts/', views.PostList.as_view(), name='posts_index'),
+    path('posts/new/', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:post_id>', views.show, name='show'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
+]
