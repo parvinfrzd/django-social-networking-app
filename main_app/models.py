f20f68511742fb90a1203605d5ff353cd4ea61b3
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-15 08:39:49 -0500

    Add profile and user model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date 
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+
+
+COUNTRY = 0
+CITY = 1
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    bio = models.TextField(null=True)
+    birth_date = models.DateField(null=True)
+    follows = models.ManyToManyField(
+        "self", 
+        related_name="followed_by", 
+        symmetrical=False, 
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+# class Location(models.Model):
+    
+#     LOCATION_TYPE_CHOICES = ((COUNTRY, 'country'), (CITY, 'city'))
+
+#     profile = models.ForeignKey('Profile', ..., related_name='locations')
+#     name = models.CharField(...)
+#     type = models.IntegerField(..., choices=LOCATION_TYPE_CHOICES)
+
+#     class Meta:
+#         unique_together = ('profile', 'type')
+        
+        
