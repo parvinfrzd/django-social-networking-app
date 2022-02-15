0b0e9f4c85551f635601958377b7d59b4ecf7806
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-15 08:40:31 -0500

    Add main application urls and paths

---
+++ main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+
+app_name = "social"
+
+urlpatterns = [
+    path("", dashboard, name="dashboard"),
+    path('profile_list/',profile_list, name='profile_list'),
+    path('profile/<int:pk>', profile, name='profile'),
+    path('accounts/signup/', signup, name='signup'),
+
+]
