9c6aa4b489ed6d70eb655b094728380047ac06be
Dylan <dburston0@gmail.com>
2022-02-17 07:36:03 -0500

    added comment crud:

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('posts/', views.PostList.as_view(), name='posts_index'),
     path('posts/new/', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/<int:post_id>/add_comment/', views.add_comment, name='add_comment'),
+    path('posts/<int:post_id>/comments/<int:comment_id>', views.delete_comment, name='comment_delete'),
     path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
     path('posts/<int:post_id>', views.show, name='show'),
     path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
+    path('', views.index, name="index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .forms import CommentForm
 
-from main_app.models import Post
+from main_app.models import Post, Comment
 
 class PostList(ListView):
   model = Post
@@ -21,6 +22,27 @@ class PostDelete(DeleteView):
   success_url = '/posts/'
 
 def show(request, post_id):
+    print("in show")
     post = Post.objects.get(id=post_id)
-    return render(request, 'show.html', {'post': post})
+    comment_form = CommentForm()
+    return render(request, 'show.html', {'post': post, 'comment_form': comment_form})
 
+def add_comment(request, post_id):
+  print("in add_comment")
+	# create the ModelForm using the data in request.POST
+  form = CommentForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_comment = form.save(commit=False)
+    new_comment.post_id = post_id
+    new_comment.save()
+  return redirect('show', post_id=post_id)
+
+def delete_comment(request, post_id, comment_id):
+  Comment.objects.get(id=comment_id).delete()
+  return redirect('show', post_id=post_id)
+
+def index(request):
+  return redirect('/posts')
