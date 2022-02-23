550d6531b057a758e68da2043815206fcbba7180
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-22 21:55:34 -0500

    swap base_dir

--- social_network/settings.py
+++ social_network/settings.py
@@ -24,9 +24,9 @@ environ.Env.read_env()
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 WSGI_APPLICATION = 'social_network.wsgi.application'
 
